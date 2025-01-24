af9dd5aabed755acfc01644d31d45dd770b02ce4
Othavio <othavionogueira2003@gmail.com>
2025-01-24 15:04:17 -0300

    Renomeando pastas do projeto

---
+++ SiteEmMVC/SiteEmMVC/Models/HomeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SistemaEmMVC.Models
+{
+    public class HomeModel
+    {
+        public string Nome { get; set; }
+        
+        public string Email { get; set; }
+    }
+}
